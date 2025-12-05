e4c7344e5100d41d4fd8b1c115865095815501de
G. Assumani <167922912+Steam90902@users.noreply.github.com>
2025-12-05 13:33:13 +0000

    finished making a stack

---
+++ c++/ClassProj/StackProj/Stack.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+int stack[5] = {};
+int pointer = 0;
+
+void push(int value);
+void peek();
+int pop();
+
+int main(){
+    int opt;
+    int tempVal;
+    std::cout << "404 = leave \n 1 = push \n 2 = pop \n 3 = peek \n";
+    while(opt != 404){
+        std::cout << "Your option: ";
+        std::cin >> opt;
+        std::cout << "\n";
+        if(opt == 1){
+            std::cout << "push what: ";
+            std::cin >> tempVal;
+            push(tempVal);
+        }
+        else if(opt == 2){
+            std::cout << "Taken out :" << pop() << "\n";
+        }
+
+        else if(opt == 3){
+            peek();
+        }
+
+        else if(opt == 404){
+            break;
+        }
+
+        else{
+            std::cout << "Enter a valid command";
+
+        }
+
+        std::cout << "\n";
+    }
+
+
+    return 0;
+}
+
+void push(int value){
+    stack[pointer] = value;
+    pointer += 1;
+}
+
+int pop(){
+    int temp = stack[pointer -1];
+    pointer -= 1;
+    return temp;
+}
+
+void peek(){
+    std::cout << stack[pointer -1 ] << " is next in the stack\n";
+}
