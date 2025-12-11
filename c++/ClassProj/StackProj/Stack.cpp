35f14e70117ec4f4fa0896bd4c473443f9290ca8
Steam90902 <gabassumani@gmail.com>
2025-12-11 12:38:55 +0000

    finished stacks properly

--- c++/ClassProj/StackProj/Stack.cpp
+++ c++/ClassProj/StackProj/Stack.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-int stack[5] = {};
+int stack[6] = {5, 5, 5, 5, 5};
 int pointer = 0;
+
 
 void push(int value);
 void peek();
@@ -15,29 +16,26 @@ int main(){
         std::cout << "Your option: ";
         std::cin >> opt;
         std::cout << "\n";
-        if(opt == 1){
-            std::cout << "push what: ";
-            std::cin >> tempVal;
-            push(tempVal);
-        }
-        else if(opt == 2){
-            std::cout << "Taken out :" << pop() << "\n";
-        }
 
-        else if(opt == 3){
-            peek();
+        switch (opt) {
+            case 1:
+                std::cout << "push what: ";
+                std::cin >> tempVal;
+                push(tempVal);
+                break;
+            
+            case 2:
+                std::cout << "Taken out :" << pop() << "\n";
+                break;
+            
+            case 3:
+                peek();
+                break;
+            default:
+                std::cout << "Enter a valid command";
         }
-
-        else if(opt == 404){
-            break;
-        }
-
-        else{
-            std::cout << "Enter a valid command";
-
-        }
-
-        std::cout << "\n";
+        
+        std::cout << "\n Pointer is " << pointer << "\n";
     }
 
 
@@ -45,33 +43,34 @@ int main(){
 }
 
 void push(int value){
-    if(pointer >= 5){
-        std::cout << "Stack Overflow\n";
-    }
-    else{
+    if(pointer >= 0 && pointer < 5){
         stack[pointer] = value;
         pointer += 1;
+    }
+    else{
+        std::cout << "Error";
     }
 }
 
 int pop(){
-    if(pointer >= 0){
-        std::cout << "Stack Underflow\n";
-        return 404;
-    }
-    else{
+    if(pointer > 0 && pointer <= 5){
         int temp = stack[pointer -1];
         pointer -= 1;
         return temp;
-    } 
+    }
+    
+    else{
+        return 404;
+    }
+        
+    
 }
 
 void peek(){
-    if(pointer >= 0 ){
-        std::cout << "Stack Underflow";
-    }
-    else{
+    if(pointer > 0 && pointer <= 5){
         std::cout << stack[pointer -1 ] << " is next in the stack\n";
     }
-   
+    else{
+        std::cout << "Error";
+    }
 }
