83792e7a97f78d21fb3e0dc397177e148e92884c
Steam90902 <gabassumani@gmail.com>
2026-01-16 10:44:45 +0000

    The variable are actually saved in two separate arrays now, one for the char and one for the value. Next ill put functions for the equations

--- c++/TestingC++.cpp
+++ c++/TestingC++.cpp
@@ -1,82 +1,71 @@
 #include <iostream>
+#include <format>
 
-
+char kineVariables[4];
+float kineValues[4] = {0.0};
 
 int main(){
-    float a;
-    float u;
-    float v;
-    float t;
-    float s;
-    std::string missingTemp;
-    std::string variableTemp;
+
+
     std::cout << "This code will only work properly if the object experience a force/acceleration.\n";
     
     // Find out what variables are missing
 
     std::cout << "What variable are you looking for?: ";
-
-
-
-    std::cin >> missingTemp;
-
-    
-
+    std::cin >> kineVariables[0];
 
     // Input the variables
 
-
-    std::cout << "What variables do you have (you need three): ";
-    for(int i = 0; i < 3 ; i++){
-        std::cout << "Orange";
+    std::cout << "Here, youll have to input the variables you have (you need three)\n";
+    for(int i = 1; i < 4 ; i++){
+        //std::cout << "Orange";
         char Temp;
-        std::cout << "What is the missing variable: ";
+        std::cout << "What is variable no " << i << ": ";
         std::cin >> Temp;
+        std::cout << "\n";
         switch (Temp)
         {
         case 'a':
+            kineVariables[i] = 'a';
             std::cout << "\nWhat is the Acceleration: ";
-            std::cin >> a;
+            std::cin >> kineValues[i];
             break;
             
         case 'u':
+            kineVariables[i] = 'u';
             std::cout << "\nWhat is the initial Velocity?: ";
-            std::cin >> u;
+            std::cin >> kineValues[i];
             break;
 
         case 'v':
+            kineVariables[i] = 'v';
             std::cout << "\nWhat is the final Velocity?: ";
-            std::cin >> v;
+            std::cin >> kineValues[i];
             break;
 
         case 's':
+            kineVariables[i] = 's';
             std::cout << "\nWhat is the Displacement?: ";
-            std::cin >> s;
+            std::cin >> kineValues[i];
             break;
 
         case 't':
+            kineVariables[i] = 't';
             std::cout << "\nWhat is the Time?: ";
-            std::cin >> t;
+            std::cin >> kineValues[i];
             break;
 
         default:
             break;
         }
+        std::cout << "\n";
     }
-    std::cout << "What is the Acceleration: ";
-    std::cin >> a;
+    std::cout << "Youll get the missing variable soon :D\n";
 
-    std::cout << "\nWhat is the initial Velocity?: ";
-    std::cin >> u;
+    for(int i = 1; i < 4; i++){
+        std::cout << kineVariables[i] << " with " <<  kineValues[i] << "\n";
+    }
 
-    std::cout << "\nWhat is the final Velocity?: ";
-    std::cin >> v;
-
-    std::cout << "\nWhat is the Time?: ";
-    std::cin >> t;
-
-    std::cout << "\nWhat is the Displacement?: ";
-    std::cin >> s;
 
     return 0;
 }
